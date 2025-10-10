c1008a5bc12ae243fa8d72c78c22189ea5ffa720
Hemant <125254650+Hemant8080@users.noreply.github.com>
2025-10-10 11:34:54 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,13 @@ import { getStorage } from 'firebase/storage'
 
 // TODO: Replace with your Firebase project config (from Firebase Console)
 const firebaseConfig = {
-    apiKey: "AIzaSyAG1_zy1po1TEbsTKw5tCKNIAfUw9NEq0A",
-    authDomain: "cloud-storage-eab72.firebaseapp.com",
-    projectId: "cloud-storage-eab72",
-    storageBucket: "cloud-storage-eab72.firebasestorage.app",
-    messagingSenderId: "486774340784",
-    appId: "1:486774340784:web:0c20905f33b4ed590acf75",
-    measurementId: "G-FVPJ4JEP7J"
-  };
+  apiKey: "your-api-key",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id"
+};
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
